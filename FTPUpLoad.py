63f310cb16b9dba3ded8f5819fb9946f75186a02
fierrose <fierrose@naver.com>
2019-06-19 11:36:28 +0900

    init

--- FTPUpLoad.py
+++ FTPUpLoad.py
@@ -4,3 +4,13 @@
 import os
 import sys
 import ftplib
+import  glob
+from functools import wraps
+import zipfile
+from time import sleep, time
+import platform
+import logging
+import re
+
+
+from ./env import enverment
